fa6f6da20dc3d212b60346594b5ab2f1636230eb
Sudam Fernando <sudam17fernando@gmail.com>
2025-07-24 13:02:42 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -1,1 +1,21 @@
-console.log("bye");
+//ENNGswYJaHT1PtH9
+
+const express = require("express");
+const mongoose = require("mongoose");
+
+
+const app = express();
+
+//Middleware
+app.use("/",(req, res, next) => {
+    res.send("It is working")
+})
+
+
+
+mongoose.connect("mongodb+srv://admin:ENNGswYJaHT1PtH9@cluster0.mjltbxo.mongodb.net/")
+.then(()=> console.log("Connected to MongoDB"))
+.then(() => {
+    app.listen(5000);
+})
+.catch((err) => console.log((err)));
