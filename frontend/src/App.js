0beb86cef53822ab201c98de0d3170bcfde0598e
rashmikanirml <rashmikanirml@gmail.com>
2025-08-12 10:28:07 +0530

    edits done backend for register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,14 @@
 import React from "react";
-import { Route, Routes} from "react-router";
+import { Route, Routes } from "react-router";
 import "./App.css";
 import Register from "./Components/Register/Register";
 import Login from "./Components/Login/Login";
 import Welcome from "./Components/Welcome/Welcome";
 import HRDashboard from "./Components/Dashboards/HRDashboard";
 import SupplierDashboard from "./Components/Dashboards/SupplierDashboard";
-import WholesalerDashboard from "./Components/Dashboards/WholesalerDashboard"
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
+import WholesalerDashboard from "./Components/Dashboards/WholesalerDashboard";
 import Delivery from "./Components/Delivery/Delivery";
-=======
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 import Supplier from "./Components/Supplier/Supplier";
-
->>>>>>> Stashed changes
 
 function App() {
   return (
@@ -31,21 +21,8 @@ function App() {
           <Route path="/hr-dashboard" element={<HRDashboard />} />
           <Route path="/supplier-dashboard" element={<SupplierDashboard />} />
           <Route path="/wholesaler-dashboard" element={<WholesalerDashboard />} />
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
           <Route path="/delivery-vans" element={<Delivery />} />
-          
-
-=======
           <Route path="/supplier" element={<Supplier />} />
->>>>>>> Stashed changes
-=======
-          <Route path="/supplier" element={<Supplier />} />
->>>>>>> Stashed changes
-=======
-          <Route path="/supplier" element={<Supplier />} />
->>>>>>> Stashed changes
         </Routes>
       </React.Fragment>
     </div>
