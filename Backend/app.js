49bd7c8f8464366c6c1eec8795b961da15f4d7b6
Sudam Fernando <sudam17fernando@gmail.com>
2025-08-30 09:52:45 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -14,13 +14,13 @@ app.use(cors());
 app.use(express.json()); // for parsing application/json
 
 
-// MongoDB Connection (Cloud Mongo URI or Localhost)
+// MongoDB Connection
 mongoose.connect("mongodb+srv://admin:ENNGswYJaHT1PtH9@cluster0.mjltbxo.mongodb.net/teafactory", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
-.then(() => console.log("✅ MongoDB connected"))
-.catch((err) => console.error("❌ MongoDB connection error:", err));
+.then(() => console.log("MongoDB connected"))
+.catch((err) => console.error("MongoDB connection error:", err));
 
 // Routes
 app.use("/api/users", userRoutes); // All user/login/register routes
@@ -29,19 +29,19 @@ app.use('/api/schedules', scheduleRoutes);
 
 // Root route for testing in Postman
 app.get("/", (req, res) => {
-  res.send("API is running 🚀");
+  res.send("API is running");
 });
 
 // Root route for testing in Postman
 app.get("/", (req, res) => {
-  res.send("API is running 🚀");
+  res.send("API is running");
 });
 
 // Root route for testing in Postman
 app.get("/", (req, res) => {
-  res.send("API is running 🚀");
+  res.send("API is running");
 });
 
 // Start the server
 const PORT = 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
